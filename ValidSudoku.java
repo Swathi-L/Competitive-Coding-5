1eac56d41fb88c417df1c99e8b535e682616422c
Swathi Loganathan <swathijubilant@gmail.com>
2025-08-26 23:41:33 -0700

    Completed coding-5

---
+++ ValidSudoku.java
@@ -0,0 +1,62 @@
+// Time Complexity : O(1), since matrix size is always 9*9
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+// Approach:
+// 1. Iterate over each row and col to check for duplicate num, use hash set to check duplicate
+// 2. Iterate over each 3*3 grid to check for duplicate
+// 3. Return true if no duplicate else false
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+
+        for(int i=0; i<9; i++){
+            Set<Character> charSet = new HashSet<>();
+            Set<Character> colSet = new HashSet<>();
+
+            for(int j=0; j<9; j++) {
+                char c = board[i][j];
+                // row check
+                if(c != '.' && charSet.contains(c)) {
+                    return false;
+                } else if(c != '.' && !charSet.contains(c)) {
+                    charSet.add(c);
+                }
+
+                // col check
+                c = board[j][i];
+                if(c != '.' && colSet.contains(c)) {
+                    return false;
+                } else if(c != '.' && !colSet.contains(c)) {
+                    colSet.add(c);
+                }
+            }
+        }
+
+        // 0 0, 1 0, 2 0, 3 0, 4 0...
+        // 0 1  11 2 1 3 1
+        // r = 0 1 2, c = 0 1 2
+        // r 0 1 2    c = 3 4 5, 6 7 8
+        // r 3 4 5
+
+        for(int i = 0; i < 9; i += 3){
+            for(int j=0; j<9; j+= 3) {
+                Set<Character> gridSet = new HashSet<>();
+                for(int r = i; r < i+3; r++) {
+                    for(int c = j; c < j+3; c++) {
+                        char ch = board[r][c];
+                        if(ch != '.' && gridSet.contains(ch)) {
+                            return false;
+                        } else if(ch != '.' && !gridSet.contains(ch)) {
+                            gridSet.add(ch);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
